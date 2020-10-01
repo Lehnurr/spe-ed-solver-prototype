9b03a42c90838db94b66d77fea7a35e95eb3d277
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-01 16:34:44 +0200

    fix

--- python-visualizer/config_local.py
+++
@@ -1,6 +0,0 @@
-# -------------------------------------------------------------------
-# file for local configurations
-# defined values overwrite default values set in "config.py"
-# -------------------------------------------------------------------
-
-API_KEY = "CENSORED"
