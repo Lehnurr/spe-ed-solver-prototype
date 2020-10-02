b6bc19fa09fa214f8f4c8ddf012fe2948fabc500
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-02 20:03:52 +0200

    auto normalize all arrays for visualisation

--- python-visualizer/visualisation/GameBoardViewerDemo.py
+++ python-visualizer/visualisation/GameBoardViewerDemo.py
@@ -1,10 +1,8 @@
-
-from visualisation import GameBoardViewer
 from visualisation import SliceViewer
 import numpy as np
 
 
-WIDTH = 32
+WIDTH = 64
 HEIGHT = 32
 
 DEMO_SLICES = 50
@@ -13,7 +11,7 @@ sliceViewer = SliceViewer.SliceViewer(WIDTH, HEIGHT, ["a", "b", "c"])
 
 for sliceIdx in range(DEMO_SLICES):
     sliceViewer.add_data("a", np.ones((HEIGHT, WIDTH)) * sliceIdx)
-    sliceViewer.add_data("b", np.ones((HEIGHT, WIDTH)) * 255)
+    sliceViewer.add_data("b", np.random.randint(-100, 200, (HEIGHT, WIDTH)))
     sliceViewer.add_data("c", np.ones((HEIGHT, WIDTH)) * 30)
     sliceViewer.next_step()
 
--- python-visualizer/visualisation/SliceViewer.py
+++ python-visualizer/visualisation/SliceViewer.py
@@ -3,6 +3,9 @@ import matplotlib.pyplot as plt
 import math
 
 
+COLOR_MAP_NAME = "inferno"
+
+
 class SliceViewer:
 
     def __init__(self, plot_width, plot_height, attribute_names_list):
@@ -20,14 +23,18 @@ class SliceViewer:
         self.currentDataMapping = {}
         self.dataMappings = []
 
+        self.axMapping = {}
         self.plotMapping = {}
 
         plot_count = len(attribute_names_list)
         plots_horizontal_count = int(math.ceil(math.sqrt(plot_count)))
         plots_vertical_count = int(math.ceil(plot_count / plots_horizontal_count))
 
-        self.fig = plt.gcf()
+        self.fig = plt.figure()
         sync_axis = None
+
+        initialisation_array = np.zeros((plot_height, plot_width))
+        initialisation_array[0, 0] = 1
 
         for attribute_idx, attribute_name in enumerate(attribute_names_list):
             if not sync_axis:
@@ -38,7 +45,8 @@ class SliceViewer:
                                  sharex=sync_axis, sharey=sync_axis)
             ax.set_title(attribute_name)
             ax.xaxis.tick_top()
-            self.plotMapping[attribute_name] = ax.imshow(np.zeros((plot_height, plot_width)))
+            self.axMapping[attribute_name] = ax
+            self.plotMapping[attribute_name] = ax.imshow(initialisation_array, cmap=plt.get_cmap(COLOR_MAP_NAME))
 
     def on_scroll(self, event):
         if event.button == 'up':
@@ -49,16 +57,24 @@ class SliceViewer:
 
     def update(self):
         for attribute_name, attribute_data in self.dataMappings[self.showIdx].items():
+            assert (attribute_data is not None)
             im = self.plotMapping[attribute_name]
-            assert(attribute_data is not None)
             im.set_data(attribute_data)
+            self.axMapping[attribute_name].relim()
+            self.axMapping[attribute_name].autoscale_view()
         self.fig.suptitle(f"Slice ({self.showIdx})")
         self.fig.canvas.draw()
         self.fig.canvas.flush_events()
 
     def add_data(self, attribute_name: str, data: np.array):
         assert(attribute_name in self.attributeNamesList)
-        self.currentDataMapping[attribute_name] = data
+        max_value = np.max(data)
+        min_value = np.min(data)
+        difference = max_value - min_value
+        if difference != 0:
+            self.currentDataMapping[attribute_name] = (data + math.fabs(min_value)) / math.fabs(difference)
+        else:
+            self.currentDataMapping[attribute_name] = np.ones((self.plot_height, self.plot_width))
 
     def next_step(self):
         self.dataMappings.append(self.currentDataMapping)
