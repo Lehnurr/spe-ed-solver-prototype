7b536ef4ac11d267184484a35087489f5e91a989
SilasDerProfi <silas@schnurr.one>
2020-10-02 17:21:29 +0200

    Implemented spe_ed Simulator
    * Missing: JSON-Output generator

--- python-visualizer/simulation/spe_ed_localservice.py
+++ python-visualizer/simulation/spe_ed_localservice.py
@@ -11,6 +11,7 @@ class LocalGameService:
         self.width = width
         self.height = height
         self.cells = [0]*cell_count
+        self.round = 1
 
         for index, color in enumerate(player_colors):
             start_cell = start_point_distance * (index + 1)
@@ -27,28 +28,75 @@ class LocalGameService:
     def next_round(self) -> str:
         next_step_cells = []
         for player in self.players:
+            # apply next_action
             if (player.speed == 0 or
                     player.next_action is None or
-                    (player.next_action == PlayerAction.SLOW_DOWN and player.speed == 1) or
-                    (player.next_action == PlayerAction.SPEED_UP and player.speed == 10)):
+                    (player.next_action == Player.PlayerAction.SLOW_DOWN and player.speed == 1) or
+                    (player.next_action == Player.PlayerAction.SPEED_UP and player.speed == 10)):
                 player.speed = 0
                 player.next_action = None
                 next_step_cells.append([])
                 continue
-            elif player.next_action == PlayerAction.TURN_LEFT:
-                player.direction = PlayerDirection.LEFT if player.direction == PlayerDirection.UP else player.direction - 1
-            elif player.next_action == PlayerAction.TURN_RIGHT:
-                player.direction = PlayerDirection.UP if player.direction == PlayerDirection.LEFT else player.direction + 1
-            elif player.next_action == PlayerAction.SLOW_DOWN:
+            elif player.next_action == Player.PlayerAction.TURN_LEFT:
+                player.direction = Player.PlayerDirection.LEFT if player.direction == Player.PlayerDirection.UP else player.direction - 1
+            elif player.next_action == Player.PlayerAction.TURN_RIGHT:
+                player.direction = Player.PlayerDirection.UP if player.direction == Player.PlayerDirection.LEFT else player.direction + 1
+            elif player.next_action == Player.PlayerAction.SLOW_DOWN:
                 player.speed -= 1
-            elif player.next_action == PlayerAction.SPEED_UP:
+            elif player.next_action == Player.PlayerAction.SPEED_UP:
                 player.speed += 1
 
-            # move / set next_step_cells
-            # transfer state in cell-list / player-objects
-        # kill player that crashed because of the next action
-        # generate a JSON String with the current Game-Data.
+            jump = self.round % 6 == 0 and player.speed >= 3
 
-        for player in self.players:
+            new_position_x = player.x_position
+            new_position_y = player.y_position
+            new_position_indices = []
+
+            if player.direction == Player.PlayerDirection.UP:
+                new_position_y = self.calculate_move(player.y_position, True, player.speed)
+                for pos in range(player.y_position + 1, new_position_y):
+                    new_position_indices.append(self.get_cell_index(player.x_position, pos))
+            elif player.direction == Player.PlayerDirection.RIGHT:
+                new_position_x = self.calculate_move(player.x_position, True, player.speed)
+                for pos in range(player.x_position + 1, new_position_x + 1):
+                    new_position_indices.append(self.get_cell_index(pos, player.y_position))
+            elif player.direction == Player.PlayerDirection.DOWN:
+                new_position_y = self.calculate_move(player.y_position, False, player.speed)
+                for pos in range(new_position_y, player.y_position + 1):
+                    new_position_indices.append(self.get_cell_index(player.x_position, pos))
+            elif player.direction == Player.PlayerDirection.LEFT:
+                new_position_x = self.calculate_move(player.x_position, False, player.speed)
+                for pos in range(new_position_x + 1, player.x_position + 1):
+                    new_position_indices.append(self.get_cell_index(pos, player.y_position))
+
+            if jump:
+                while len(new_position_indices) > 2:
+                    new_position_indices.pop(1)
+
+            next_step_cells.append(new_position_indices)
             player.next_action = None
+            player.x_position = new_position_x
+            player.y_position = new_position_y
+
+            for pos in new_position_indices:
+                if self.cells[pos] == 0:
+                    self.cells[pos] = player.player_id
+                else:
+                    # Crash -> Dead
+                    player.speed = 0
+                    self.cells[pos] = -1
+                    # if already other processed players passed this cell in this round -> Kill them
+                    for player_id in range(0, player.player_id + 1):
+                        if pos in next_step_cells[player_id]:
+                            self.players[player_id].speed = 0
+
+        self.round += 1
+
+        # TODO: generate a JSON String with the current Game-Data.
         return "json"
+
+    def get_cell_index(self, x_position: int, y_position: int) -> int:
+        return y_position * self.width + x_position
+
+    def calculate_move(self, position: int, move_forward: bool, speed: int) -> int:
+        pass
