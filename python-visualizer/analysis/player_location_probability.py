2b18f6c53aaa6cc5c55fe98c2a2e44d0a708c624
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-12 23:22:35 +0200

    revert to recursive probability

--- python-visualizer/analysis/player_location_probability.py
+++ python-visualizer/analysis/player_location_probability.py
@@ -1,21 +1,20 @@
+
 from game_data.player.PlayerAction import PlayerAction
 from game_data.player.PlayerState import PlayerState
 from game_data.game.Board import Board
 import numpy as np
-from queue import Queue
-import math
 
 
 def calculate_probabilities_for_players(
         board: Board,
         player_states: [PlayerState],
-        max_depth: int,
+        depth: int,
         invalid_move_weight: float = 1) -> np.ndarray:
 
     probabilities = np.zeros((board.height, board.height))
 
     for player_state in player_states:
-        probabilities += calculate_probabilities_for_player(board, player_state, max_depth, invalid_move_weight)
+        probabilities += calculate_probabilities_for_player(board, player_state, depth, invalid_move_weight)
 
     existing_cell_occupation = np.array(board.cells)
     probabilities[existing_cell_occupation != 0] = 1
@@ -25,47 +24,33 @@ def calculate_probabilities_for_players(
 
 def calculate_probabilities_for_player(
         board: Board,
-        initial_player_state: PlayerState,
-        max_depth: int,
+        player_state: PlayerState,
+        depth: int,
         invalid_move_weight: float = 1) -> np.ndarray:
-
-    class QueueElement:
-        def __init__(self, local_depth: int, local_player_state: PlayerState):
-            self.depth = local_depth
-            self.playerState = local_player_state
 
     probabilities = np.zeros((board.height, board.height))
 
-    working_queue = Queue()
-    working_queue.put(QueueElement(0, initial_player_state))
+    for action in PlayerAction:
 
-    while not working_queue.empty():
+        new_player_state = player_state.copy()
+        new_player_state.do_action(action)
+        next_player_state = new_player_state.do_move()
 
-        queue_element = working_queue.get()
-        depth = queue_element.depth
-        player_state = queue_element.playerState
+        validity_factor = 1 / len(PlayerAction)
+        if not new_player_state.verify_move(board):
+            validity_factor *= invalid_move_weight
+        else:
+            if depth > 1:
+                recursion_result = \
+                    calculate_probabilities_for_player(board, next_player_state, depth - 1, invalid_move_weight)
+                probabilities += recursion_result * validity_factor
 
-        for action in PlayerAction:
+        affected_cells = next_player_state.steps_to_this_point
 
-            new_player_state = player_state.copy()
-            new_player_state.do_action(action)
-            next_player_state = new_player_state.do_move()
-
-            step_probability_factor = math.pow(1 / len(PlayerAction), depth)
-
-            if not new_player_state.verify_move(board):
-                step_probability_factor *= invalid_move_weight
-            else:
-                if depth < max_depth:
-                    working_queue.put(QueueElement(depth + 1, next_player_state))
-
-            affected_cells = next_player_state.steps_to_this_point
-
-            for cell_x, cell_y in affected_cells:
-                if board.point_is_on_board(cell_x, cell_y):
-                    probabilities[cell_y, cell_x] += step_probability_factor
+        for cell_x, cell_y in affected_cells:
+            if board.point_is_on_board(cell_x, cell_y):
+                probabilities[cell_y, cell_x] += validity_factor
 
     return probabilities
-
 
 
