c915c45cca50cc941b565973e6211c102daa7124
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-17 19:07:52 +0200

    improved probability based predictions to include min_steps for cells as return parameter

--- python-visualizer/players/EnemyProbabilityFullRangePlayer.py
+++ python-visualizer/players/EnemyProbabilityFullRangePlayer.py
@@ -2,7 +2,7 @@ from players.BasePlayer import BasePlayer
 from game_data.player.PlayerAction import PlayerAction
 import random
 from analysis import full_range, risk_area
-from analysis import player_location_probability
+from analysis import probability_based_prediction
 from game_data.player.PlayerState import PlayerState
 from game_data.player.PlayerState import PlayerDirection
 from game_data.game.Board import Board
@@ -45,15 +45,16 @@ class EnemyProbabilityFullRangePlayer(BasePlayer):
                         player["y"],
                         self.roundCounter))
 
-        # calculate enemy probabilities
-        enemy_probabilities = \
-            player_location_probability.calculate_probabilities_for_players(self.board, enemy_player_states, depth=5)
+        # calculate enemy prediction
+        enemy_probabilities, enemy_min_steps = \
+            probability_based_prediction.calculate_probabilities_for_players(self.board, enemy_player_states, depth=5)
+
+        # add enemy prediction to viewer
+        slice_viewer.add_data("enemy_probability", enemy_probabilities, normalize=False)
+        slice_viewer.add_data("enemy_min_steps", enemy_min_steps, normalize=True)
 
         # add risk_area to viewer
         slice_viewer.add_data("risk_evaluation", risk_area.calculate_risk_areas(self.board), normalize=False)
-
-        # add probability to viewer
-        slice_viewer.add_data("enemy_probability", enemy_probabilities, normalize=False)
 
         # apply threshold to probabilities
         enemy_probabilities[enemy_probabilities > 0.19] = 1
@@ -92,4 +93,4 @@ class EnemyProbabilityFullRangePlayer(BasePlayer):
         return action
 
     def get_slice_viewer_attributes(self):
-        return ["full_range_steps", "enemy_probability", "risk_evaluation"]
+        return ["full_range_steps", "enemy_probability", "enemy_min_steps", "risk_evaluation"]
