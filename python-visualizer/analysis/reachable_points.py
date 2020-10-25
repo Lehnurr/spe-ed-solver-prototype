3e4ede935f1e22122402f4f09a52e00170b6b6f3
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-25 12:39:54 +0100

    reachable points logic fix

--- python-visualizer/analysis/reachable_points.py
+++ python-visualizer/analysis/reachable_points.py
@@ -4,14 +4,14 @@ from game_data.player.PlayerState import PlayerState, PlayerAction
 import numpy as np
 
 
-def calculate_reachable_points(initial_player_state: PlayerState, board: Board, timeout: int = 0) -> np.ndarray:
+def calculate_reachable_points(initial_player_state: PlayerState, board: Board, timeout: float = 0.0) -> np.ndarray:
 
     reachable_points = np.zeros((board.height, board.width))
     queue = [initial_player_state]
 
     start_time = time.time()
 
-    while time.time() - start_time < timeout or timeout == 0:
+    while time.time() - start_time < timeout and timeout > 0.0:
         if len(queue) == 0:
             break
         local_base_state = queue.pop(0)
@@ -21,7 +21,6 @@ def calculate_reachable_points(initial_player_state: PlayerState, board: Board, 
             state_variation = state_copy.do_move()
             if state_variation.verify_move(board):
                 queue.append(state_variation)
-                for x, y in state_variation.steps_to_this_point:
-                    reachable_points[y, x] = 1
+                reachable_points[state_variation.position_y, state_variation.position_x] = 1
 
     return reachable_points
