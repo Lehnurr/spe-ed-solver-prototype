87569d7b5a7dc2fe71003437aa1bcc52e02c67eb
Felix Lehnerer <felix.lehnerer@web.de>
2020-10-25 12:33:07 +0100

    simplify reachable_points

--- python-visualizer/analysis/reachable_points.py
+++ python-visualizer/analysis/reachable_points.py
@@ -2,46 +2,26 @@ import time
 from game_data.game.Board import Board
 from game_data.player.PlayerState import PlayerState, PlayerAction
 import numpy as np
-from threading import Thread
 
 
-class ReachablePointsCalculation:
+def calculate_reachable_points(initial_player_state: PlayerState, board: Board, timeout: int = 0) -> np.ndarray:
 
-    def __init__(self, initial_player_state: PlayerState, board: Board):
-        self.__reachablePoints = np.zeros((board.height, board.width))
-        self.__queue = [initial_player_state]
-        self.__board = board
-        self.__running = False
-        self.__thread = Thread(target=self.__run, args=())
-        self.__thread.start()
+    reachable_points = np.zeros((board.height, board.width))
+    queue = [initial_player_state]
 
-    def __run(self):
-        while True:
-            if len(self.__queue) == 0 or not self.__running:
-                break
-            local_base_state = self.__queue.pop(0)
-            for action in PlayerAction:
-                state_copy = local_base_state.copy()
-                state_copy.do_action(action)
-                state_variation = state_copy.do_move()
-                if state_variation.verify_move(self.__board):
-                    self.__queue.append(state_variation)
-                    for x, y in state_variation.steps_to_this_point:
-                        self.__reachablePoints[y, x] = 1
+    start_time = time.time()
 
-    def stop(self):
-        self.__running = False
-        self.__thread.join()
+    while time.time() - start_time < timeout or timeout == 0:
+        if len(queue) == 0:
+            break
+        local_base_state = queue.pop(0)
+        for action in PlayerAction:
+            state_copy = local_base_state.copy()
+            state_copy.do_action(action)
+            state_variation = state_copy.do_move()
+            if state_variation.verify_move(board):
+                queue.append(state_variation)
+                for x, y in state_variation.steps_to_this_point:
+                    reachable_points[y, x] = 1
 
-    def get_result(self) -> np.ndarray:
-        return self.__reachablePoints.copy()
-
-
-def calculate_reachable_points(initial_player_state: PlayerState, board: Board, seconds_to_spend: int) -> np.ndarray:
-
-    calc = ReachablePointsCalculation(initial_player_state, board)
-    time.sleep(seconds_to_spend)
-    calc.stop()
-    res = calc.get_result()
-
-    return res
+    return reachable_points
