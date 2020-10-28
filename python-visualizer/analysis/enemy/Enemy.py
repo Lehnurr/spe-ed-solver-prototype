ee185c7e423fe7e1560d268b12a23db3da6505b1
SilasDerProfi <silas@schnurr.one>
2020-10-28 19:28:22 +0100

    Add Enemy evaluation (prototype, unfinished, untested)

---
+++ python-visualizer/analysis/enemy/Enemy.py
@@ -0,0 +1,37 @@
+from game_data.player.Player import Player
+from game_data.player.PlayerAction import PlayerAction
+from game_data.player.PlayerState import PlayerState, PlayerDirection
+
+
+class Enemy(Player):
+    def __init__(self, player_id: int, direction: PlayerDirection, speed: int, x_position: int, y_position: int):
+        super().__init__(player_id, PlayerState(direction, speed, x_position, y_position))
+
+    def update(self, step_info):
+        # compute last action
+        self.next_action = PlayerAction.CHANGE_NOTHING
+        if step_info["speed"] > self.current_state.speed:
+            self.next_action = PlayerAction.SPEED_UP
+        elif step_info["speed"] < self.current_state.speed:
+            self.next_action = PlayerAction.SLOW_DOWN
+        elif self.current_state.direction.turn(PlayerAction.TURN_LEFT).name.lower() == step_info["direction"]:
+            self.next_action = PlayerAction.TURN_LEFT
+        elif self.current_state.direction.turn(PlayerAction.TURN_RIGHT).name.lower() == step_info["direction"]:
+            self.next_action = PlayerAction.TURN_RIGHT
+
+        # do last action
+        self.do_action_and_move()
+
+    def recalculate_aggressiveness(self, enemies_states):
+        pass
+        # recalculate_aggressiveness:
+        # - speed-behavior (avg, min, max) & Number of passed cells (incl. & escl. jumped over cells)
+        # - radius of the passed cells / span of the passed cells
+
+        # - airline to the nearest player (avg, min, max, differences for all rounds)
+        #    also consider a weighted air line (to estimate the distance in rounds)
+        # - Number of taken / prevent potential collisions
+        #   collision means that in at least one case at least one player would die in the next x rounds (default x = 2)
+
+        # - Evaluate movement in risk areas
+        #   (if they often move in high-risk areas, they are willing to take a risk, or stupid)
---
+++ python-visualizer/analysis/enemy/EnemyCollection.py
@@ -0,0 +1,28 @@
+from typing import Dict
+
+from analysis.enemy.Enemy import Enemy
+from game_data.player.PlayerState import PlayerDirection
+
+
+class EnemyCollection:
+    def __init__(self):
+        self.players: Dict[int: Enemy] = {}
+
+    def update(self, step_info: dict):
+        decision_relevant_player_states = {player_id: player.current_state for player_id, player in self.players.items()}
+
+        # Update all players
+        for player_id, player_data in step_info["players"].items():
+            self.players.setdefault(player_id, Enemy(player_id,
+                                                     PlayerDirection[player_data["direction"].upper()],
+                                                     player_data["speed"],
+                                                     player_data["x"],
+                                                     player_data["y"])
+                                    ).update(player_data)
+
+        # recalculate_aggressiveness for all enemies
+        for player in self.players:
+            # only for active players and not for your own player
+            if player.is_active and player.player_id != step_info["you"]:
+                players_enemies = [v for k, v in decision_relevant_player_states.items() if k != player.player_id]
+                player.recalculate_aggressiveness(players_enemies)
