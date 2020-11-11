1fca47769365669e5756b90182fd664ae6dd4781
SilasDerProfi <silas@schnurr.one>
2020-11-11 21:15:08 +0100

    Add example visualisation for full_range recycling to RandomFullRangePlayer

--- python-visualizer/players/RandomFullRangePlayer.py
+++ python-visualizer/players/RandomFullRangePlayer.py
@@ -1,4 +1,6 @@
+from analysis.enemy.EnemyCollection import EnemyCollection
 from analysis.full_range import no_risk_full_range
+from analysis.full_range.update import update_full_range_result
 from players.BasePlayer import BasePlayer
 from game_data.player.PlayerAction import PlayerAction
 import random
@@ -14,8 +16,11 @@ class RandomFullRangePlayer(BasePlayer):
         self.roundCounter = 0
         self.board = None
         self.playerState = None
+        self.enemies = EnemyCollection()
+        self.full_range_result = None
 
     def handle_step(self, step_info, slice_viewer):
+        new_occupied_cells = self.enemies.update(step_info)
 
         self.roundCounter += 1
         own_player = step_info["players"][str(step_info["you"])]
@@ -32,10 +37,34 @@ class RandomFullRangePlayer(BasePlayer):
         # update cells
         self.board.cells = step_info["cells"]
 
+        if self.full_range_result:
+            # recycle the last full_range result
+            new_enemies_occupied_cells = [cell for cell in new_occupied_cells
+                                          if cell not in self.playerState.steps_to_this_point]
+            new_full_range_result = update_full_range_result(self.playerState.game_round,
+                                                             self.playerState.get_position_tuple(),
+                                                             self.full_range_result,
+                                                             new_enemies_occupied_cells)
+            new_path_options = [state
+                                for directions in new_full_range_result.values()
+                                for speeds in directions.values()
+                                for state in speeds.values()]
+
+            # add new path options to viewer
+            new_path_steps_array = np.zeros((step_info["height"], step_info["width"]))
+            for option in new_path_options:
+                x = option.position_x
+                y = option.position_y
+                current_value = new_path_steps_array[y, x]
+                new_value = option.game_round - self.roundCounter
+                new_path_steps_array[y, x] = new_value if current_value == 0 else min(current_value, new_value)
+
+            slice_viewer.add_data("recycled_full_range_steps", new_path_steps_array)
+
         # calculate action
-        full_range_result = no_risk_full_range.calculate_ranges_for_player(self.board, self.playerState)
+        self.full_range_result = no_risk_full_range.calculate_ranges_for_player(self.board, self.playerState)
         path_options = [state
-                        for directions in full_range_result.values()
+                        for directions in self.full_range_result.values()
                         for speeds in directions.values()
                         for state in speeds.values()]
 
@@ -66,4 +95,4 @@ class RandomFullRangePlayer(BasePlayer):
         return action
 
     def get_slice_viewer_attributes(self):
-        return ["full_range_steps"]
+        return ["full_range_steps", "recycled_full_range_steps"]
