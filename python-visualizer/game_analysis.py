ff58be8e2e217c1e1a05cc7c87c65c8c872f0a95
Felix Lehnerer <48395059+Felixleh@users.noreply.github.com>
2020-12-30 01:26:09 +0100

    game analysis console logging

--- python-visualizer/game_analysis.py
+++ python-visualizer/game_analysis.py
@@ -1,7 +1,7 @@
 import asyncio
 import json
-import random
-from datetime import datetime, time, timedelta
+import time
+from datetime import datetime, timedelta
 
 import websockets
 import requests
@@ -28,21 +28,38 @@ print(f"time offset to server: %s" % time_offset)
 def transform_to_server_time(time_value: datetime):
     return time_value + time_offset
 
-# async def play():
-#     url = config.URL
-#     key = config.API_KEY
-#
-#     game_board_viewer = None
-#
-#     async with websockets.connect(f"{url}?key={key}") as websocket:
-#         print("Waiting for initial state...", flush=True)
-#         while True:
-#             state_json = await websocket.recv()
-#             state = json.loads(state_json)
-#             print("<", state)
-#             action_json = json.dumps({"action": "change_nothing"})
-#             await websocket.send(action_json)
-#
-#
-# while True:
-#     asyncio.get_event_loop().run_until_complete(play())
+async def play():
+    url = config.URL
+    key = config.API_KEY
+
+    async with websockets.connect(f"{url}?key={key}") as websocket:
+        started = False
+        print("Waiting for initial state...", flush=True)
+
+        while True:
+            state_json = await websocket.recv()
+            state = json.loads(state_json)
+
+            if not started:
+                print(f"board -- width: %d -- height: %d" % (state["width"], state["height"]))
+                started = True
+
+            if not state["running"]:
+                break
+
+            deadline = datetime.strptime(state["deadline"], '%Y-%m-%dT%H:%M:%SZ')
+            available_millis = (deadline - transform_to_server_time(datetime.now())) / timedelta(milliseconds=1)
+
+            print(f"\tavailable milliseconds: %d" % available_millis)
+
+            time.sleep(0.5)
+
+            action_json = json.dumps({"action": "change_nothing"})
+            await websocket.send(action_json)
+
+
+while True:
+    try:
+        asyncio.get_event_loop().run_until_complete(play())
+    except Exception:
+        print("Connection was interrupted!")
